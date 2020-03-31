071c30db70bb2a809699351f43aa3b9952e04b6a
LZ <liuzhellll@hotmail.com>
2020-03-31 18:03:53 +0800

    添加项目文件。

---
+++ WebApplication/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+#if !DEBUG
+                    webBuilder.UseUrls("http://0.0.0.0:5006/"); // 或者使用"http://*:5000"不这么设置腾讯云不能运行          
+#endif
+                });
+    }
+}
