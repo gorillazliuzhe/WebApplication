917e0fb7a0d19b8d7165a6d62ce5a611e1b6224b
liuzhe <liuzhellll@hotmail.com>
2021-08-31 15:38:46 +0800

    升级5

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -55,7 +55,7 @@ namespace WebApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Bing}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
